603cc1ad226070dd441e02b29653b86359b4e2bb
Alexandre Luís <alexandreluisrigotti@gmail.com>
2021-10-08 10:31:25 -0300

    Projeto Integrador - Fase 1: Arquivos de projeto

---
+++ Estacionamento/src/br/com/estacionamento/model/VagaOcupada.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.estacionamento.model;
+
+/**
+ *
+ * @author alexandreluis
+ */
+public class VagaOcupada
+{
+    int idVagas;
+    int codigoDaVaga;
+    int controleVaga_idControleVaga;
+    int controleVaga_controlePagamento_idControlePagamento; 
+}
